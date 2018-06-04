553b009035bf2d47d3589b9e526bf04d07841340
jisuAN <ajs7270@naver.com>
2018-06-04 13:51:47 +0900

    add CLOSURE function

--- src/com/parser/Parser.java
+++ src/com/parser/Parser.java
@@ -1,24 +1,46 @@
 package com.parser;
 
+import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Parser {
 
 	public static void main(String[] args) {
 
 		LinkedList<LinkedList<String>> C0 = new LinkedList<LinkedList<String>>();
-
+		
 		FileIO file = new FileIO();
 		file.load();
 
 		LinkedList<String> CFG = (LinkedList<String>) file.getRule();
+		
 	}
 
 	public List<String> CLOSURE(List<String> rule) {
+		Iterator<String> iter = rule.iterator();
 		List<String> result = new LinkedList<String>();
-
-		return rule;
+		Queue<String> queue = new LinkedList<String>();
+		String cfg = null;
+		while(iter.hasNext()) {
+			queue.offer(iter.next());
+			while(!queue.isEmpty())
+				cfg = queue.poll();
+				// '.' 이 없는 경우 '.'을 추가하여 result와 queue에 넣어줌
+				if(!cfg.contains(".")) {
+					StringBuffer cfgPlusDotSymbol = new StringBuffer();
+					cfgPlusDotSymbol.append(cfg);
+					cfgPlusDotSymbol.insert(cfg.indexOf('>') + 1,".");
+					System.out.println(cfgPlusDotSymbol);
+					queue.offer(cfgPlusDotSymbol.toString());
+					result.add(cfgPlusDotSymbol.toString());
+				}else {
+					
+				}
+		}
+		return result;
 	}
 
 	public Boolean isTerminal(String rule) {
@@ -38,7 +60,5 @@ public class Parser {
 			return true;
 		}
 	}
-	
-	public 
 
 }
